7da7d8cb31f8c5287ab99625220dc57c1ac6871a
ahmed <61820697+ahmed-fayyaz-dev@users.noreply.github.com>
2022-04-01 21:52:36 +0500

    formikized

--- src/screens/myOrders/components/changeOrderStatusDropdown.jsx
+++ src/screens/myOrders/components/changeOrderStatusDropdown.jsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 import { CustomDropDownPicker } from 'src/components/dropDownPicker';
 import { orderStatusArr } from 'src/screens/myOrders/dataFormat';
 
-export default function Playground({ selectedValue, onChange }) {
+export default function ChangeOrderStatusDropdown({ selectedValue, onChange }) {
     const [value, setValue] = useState(selectedValue);
 
     function handleChange(v) {
